d2d4c7e1df9b3b08f97e35d553b72ee3d11b7f57
Andrew Marshall <marshall.a.f@gmail.com>
2018-10-20 16:04:33 -0500

    Add logout route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,12 @@ module.exports = (app) => {
 
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send({ currentUser: req.user });
+  });
+
   app.get('/api/current_user', (req, res) => {
-    res.send(req.user);
+    res.send({ currentUser: req.user });
   });
 };
