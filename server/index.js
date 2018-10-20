584dd00a6c7663c4f272268690a0287af1f9ef1d
Andrew Marshall <marshall.a.f@gmail.com>
2018-10-20 15:43:37 -0500

    Use cookie session for user auth

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
 
@@ -9,6 +11,16 @@ require('./services/passport');
 mongoose.connect(keys.mongoUri);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in ms
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
