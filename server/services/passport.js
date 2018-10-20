393c667721bb13a8c5f0b9e2345d866c0f8febaf
Andrew Marshall <marshall.a.f@gmail.com>
2018-10-20 15:08:45 -0500

    Save user to db

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,13 +1,16 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+// using this way (instead of require) prevents multiple
+// includes of 'users', esp. in testing scenarios
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-  console.log('access token:', accessToken);
-  console.log('refresh token:', refreshToken);
-  console.log('profile:', profile);
+  new User({ googleId: profile.id }).save();
 }));
